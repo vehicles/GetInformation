8682ec97b455ada4c7ec3ce6d0c798acc0c8e689
Vehicles <vehicles@users.noreply.github.com>
2019-09-17 08:23:32 +0700

    Add files via upload

---
+++ GetTaxRate/GetTaxRate/GetTaxRate.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GetTaxRate
+{
+  partial class frmMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnGetTaxRate = new System.Windows.Forms.Button();
+      this.lblMST = new System.Windows.Forms.Label();
+      this.txtMSTDN = new System.Windows.Forms.TextBox();
+      this.txtThongTinDN = new System.Windows.Forms.TextBox();
+      this.lbThongTinDN = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // btnGetTaxRate
+      // 
+      this.btnGetTaxRate.Location = new System.Drawing.Point(713, 6);
+      this.btnGetTaxRate.Name = "btnGetTaxRate";
+      this.btnGetTaxRate.Size = new System.Drawing.Size(75, 23);
+      this.btnGetTaxRate.TabIndex = 2;
+      this.btnGetTaxRate.Text = "&Xử lý";
+      this.btnGetTaxRate.UseVisualStyleBackColor = true;
+      this.btnGetTaxRate.Click += new System.EventHandler(this.btnGetTaxRate_Click);
+      // 
+      // lblMST
+      // 
+      this.lblMST.AutoSize = true;
+      this.lblMST.Location = new System.Drawing.Point(12, 9);
+      this.lblMST.Name = "lblMST";
+      this.lblMST.Size = new System.Drawing.Size(128, 13);
+      this.lblMST.TabIndex = 1;
+      this.lblMST.Text = "Mã số thuế doanh nghiệp";
+      // 
+      // txtMSTDN
+      // 
+      this.txtMSTDN.Location = new System.Drawing.Point(146, 6);
+      this.txtMSTDN.Name = "txtMSTDN";
+      this.txtMSTDN.Size = new System.Drawing.Size(161, 20);
+      this.txtMSTDN.TabIndex = 1;
+      // 
+      // txtThongTinDN
+      // 
+      this.txtThongTinDN.Location = new System.Drawing.Point(15, 57);
+      this.txtThongTinDN.Multiline = true;
+      this.txtThongTinDN.Name = "txtThongTinDN";
+      this.txtThongTinDN.ReadOnly = true;
+      this.txtThongTinDN.Size = new System.Drawing.Size(773, 381);
+      this.txtThongTinDN.TabIndex = 3;
+      // 
+      // lbThongTinDN
+      // 
+      this.lbThongTinDN.AutoSize = true;
+      this.lbThongTinDN.Location = new System.Drawing.Point(12, 41);
+      this.lbThongTinDN.Name = "lbThongTinDN";
+      this.lbThongTinDN.Size = new System.Drawing.Size(153, 13);
+      this.lbThongTinDN.TabIndex = 4;
+      this.lbThongTinDN.Text = "THÔNG TIN DOANH NGHIỆP";
+      // 
+      // frmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.lbThongTinDN);
+      this.Controls.Add(this.txtThongTinDN);
+      this.Controls.Add(this.txtMSTDN);
+      this.Controls.Add(this.lblMST);
+      this.Controls.Add(this.btnGetTaxRate);
+      this.Name = "frmMain";
+      this.Text = "Lấy thông tin doanh nghiệp";
+      this.Load += new System.EventHandler(this.frmMain_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnGetTaxRate;
+    private System.Windows.Forms.Label lblMST;
+    private System.Windows.Forms.TextBox txtMSTDN;
+    private System.Windows.Forms.TextBox txtThongTinDN;
+    private System.Windows.Forms.Label lbThongTinDN;
+  }
+}
+
---
+++ GetTaxRate/GetTaxRate/GetTaxRate.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace GetTaxRate
+{
+  public partial class frmMain : Form
+  {
+    public frmMain()
+    {
+      InitializeComponent();
+      
+    }
+
+    private void btnGetTaxRate_Click(object sender, EventArgs e)
+    {
+      WebClient client = new WebClient();
+      client.Encoding = System.Text.Encoding.UTF8;
+
+      
+      var Json1 = client.DownloadString("https://thongtindoanhnghiep.co/api/company/" + txtMSTDN.Text);
+
+      XNode node = JsonConvert.DeserializeXNode(Json1, "Root");
+
+      #region "Write File"
+      var dir = @"D:\TuanT\Console\GetTaxrate\GetTaxRate\GetTaxRate\Log";  // folder location
+
+      if (!Directory.Exists(dir))  // if it doesn't exist, create
+        Directory.CreateDirectory(dir);
+
+          XmlDocument xmlDoc = new XmlDocument();
+      xmlDoc.LoadXml(node.ToString());
+
+      XmlWriterSettings settings = new XmlWriterSettings();
+      settings.Indent = true;
+      // Save the document to a file and auto-indent the output.
+      XmlWriter writer = XmlWriter.Create(Path.Combine(dir, txtMSTDN.Text + ".xml"), settings);
+      xmlDoc.Save(writer);
+      #endregion
+
+      #region "XPath"
+      XmlNodeList itemNodes = xmlDoc.SelectNodes("Root");
+      foreach (XmlNode itemNode in itemNodes)
+      {
+        var s = "";
+        s = "1. Mã số thuế: " + itemNode.SelectSingleNode("MaSoThue").InnerText + Environment.NewLine;
+        s += "2. Tên người nộp thuế: " + itemNode.SelectSingleNode("Title").InnerText + Environment.NewLine;
+        s += "3. Địa chỉ công ty: " + itemNode.SelectSingleNode("DiaChiCongTy").InnerText + Environment.NewLine;
+        s += "4. Chủ sở hữu: " + itemNode.SelectSingleNode("ChuSoHuu").InnerText + Environment.NewLine;
+        s += "4.1 Địa chỉ: " + itemNode.SelectSingleNode("ChuSoHuu_DiaChi").InnerText + Environment.NewLine;
+        s += "5. Nơi đăng ký kinh doanh: " + itemNode.SelectSingleNode("NoiDangKyQuanLy_CoQuanTitle").InnerText + Environment.NewLine;
+        s += "6. Giấy phép kinh doanh: " + itemNode.SelectSingleNode("GiayPhepKinhDoanh_CoQuanCapTitle").InnerText + Environment.NewLine;
+        
+        s += "7. Cấp - Chương - Loại - Khoản: " + itemNode.SelectSingleNode("CapChuongLoaiKhoan").InnerText + Environment.NewLine;
+
+        txtThongTinDN.Text = s;
+      }
+      //Load các tag li trong tag ul
+      //var threadItems = node.DocumentNode.SelectNodes("//ul[@id='threads']/li").ToList();
+
+
+      //var items = new List<object>();
+      //foreach (var item in threadItems)
+      //{
+      //  //Extract các giá trị từ các tag con của tag li
+      //  var linkNode = item.SelectSingleNode(".//a[contains(@class,'title')]");
+      //  var link = linkNode.Attributes["href"].Value;
+      //  var text = linkNode.InnerText;
+      //  var readCount = item.SelectSingleNode(".//div[@class='folTypPost']/ul/li/b").InnerText;
+
+
+      //  items.Add(new { text, readCount, link });
+
+      //}
+      #endregion
+
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+      txtMSTDN.Text = "0312692740";
+    }
+  }
+}
